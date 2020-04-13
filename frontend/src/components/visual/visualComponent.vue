ce99e2ba9db2346edab62a85b946fa6f1b1d8417
sungjong <tjdwhd3345@gmail.com>
2020-04-14 01:37:19 +0900

    mozzi / Main/VisualComponent 초기

---
+++ frontend/src/components/visual/visualComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="visual">
+      <img src="~@/assets/images/happyHouse.png" class="visual_img" alt="비주얼 이미지"><img/>
+    </div>
+    <div class="searchBox_back"></div>
+    <div class="searchBox">
+      <form action="#" name="searchForm" class="searchForm">
+        <input type="text" name="keyword" placeholder="관심지역 또는 매물번호를 입력해 주세요">
+        <input type="submit" value="매물검색">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'visualComponent'
+};
+</script>
+
+<style scoped>
+  .searchBox_back {
+    height: 110px;
+    background-color: rgba(0,0,0,0.2);
+  }
+  .searchBox {
+    width: 550px;
+    margin: 0px auto;
+  }
+</style>
