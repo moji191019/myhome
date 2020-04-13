ac128b5428490377e5bbc9abb11a7c0a11932dc8
JiYoonKim <badjiyoon1@gmail.com>
2020-04-14 00:13:11 +0900

    김지윤 / NA / NA / 메뉴 작업

--- frontend/src/components/header/headerComponent.vue
+++ frontend/src/components/header/headerComponent.vue
@@ -5,7 +5,11 @@
         <img src="~@/assets/images/logo.png" class="log" alt="로고"><img/>
       </a>
       <div class="menu">
-        menu
+        <ul>
+          <li v-for="(item, idx) in childMenu" :key="'a' + idx">
+            <a> {{item.text}} </a>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -15,6 +19,28 @@
 
 export default {
   name: 'headerComponent',
+
+  computed: {
+    childMenu() {
+      console.log('childMenu');
+      const menuArr = [];
+
+      for (let i = 0; i < 3; i += 1) {
+        menuArr.push({ text: `aaa${i}` });
+      }
+      console.log('menuArr', menuArr);
+      return menuArr;
+    },
+  },
+
+  methods: {
+
+  },
+
+  data() {
+    return {
+    };
+  },
 };
 </script>
 
