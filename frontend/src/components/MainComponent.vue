8ca1273cf9534079d6da5bf2a06c98f92199324c
JiYoonKim <badjiyoon1@gmail.com>
2020-04-14 20:10:07 +0900

    김지윤 / yarn dev 시 open browser 및 eslint 수정

--- frontend/src/components/MainComponent.vue
+++ frontend/src/components/MainComponent.vue
@@ -22,7 +22,7 @@ export default {
   },
   created() {
     this.$store.dispatch('FETCH_NEWS');
-  }
+  },
 };
 </script>
 
--- frontend/src/components/visual/visualComponent.vue
+++ frontend/src/components/visual/visualComponent.vue
@@ -16,7 +16,7 @@
 <script>
 
 export default {
-    name: 'visualComponent'
+  name: 'visualComponent',
 };
 </script>
 
