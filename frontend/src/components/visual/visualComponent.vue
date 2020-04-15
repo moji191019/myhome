65777f63511ff7530ba56c56eaa1d65a0bfe9576
mosj <tjdwhd3345@gmail.com>
2020-04-16 01:50:34 +0900

    mozzi / onclick submit, keyword onkeyup

--- frontend/src/components/visual/visualComponent.vue
+++ frontend/src/components/visual/visualComponent.vue
@@ -1,14 +1,20 @@
 <template>
-  <div>
+  <div style="position: relative;">
     <div class="visual">
       <img src="~@/assets/images/happyHouse.png" class="visual_img" alt="비주얼 이미지"><img/>
     </div>
-    <div class="searchBox_back"></div>
-    <div class="searchBox">
-      <form action="#" name="searchForm" class="searchForm">
-        <input type="text" name="keyword" placeholder="관심지역 또는 매물번호를 입력해 주세요">
-        <input type="submit" value="매물검색">
-      </form>
+    <div class="searchBox_layer">
+      <div class="searchBox_bg">
+          <div class="searchBox">
+            <form action="#" class="searchForm" ref="searchForm">
+              <div class="searchBox_inputGroup">
+                <input type="text" autocomplete="off" placeholder="관심지역 또는 매물번호를 입력해 주세요" ref="keyword" v-on:keyup="getKeyword">
+                <!-- <input type="submit" value="매물검색"> -->
+                <span class="btn" v-on:click="submit">매물 검색</span>
+              </div>
+            </form>
+          </div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,8 +23,19 @@
 
 export default {
   name: 'visualComponent',
+  methods: {
+    getKeyword() {
+      console.log(event, this.$refs.keyword.value);
+      return this.$refs.keyword.value;
+    },
+    submit(event) {
+      console.log('submit event: ', event, this);
+      this.$refs.searchForm.submit();
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style lang="scss">
+
 </style>
