ea12b4210bea897f1199096dddd818eef29e7b67
mosj <tjdwhd3345@gmail.com>
2020-04-16 19:45:28 +0900

    mozzi / background-image url 주는 방법고민해봐야 함. 서치이미지 추가.

--- frontend/src/components/visual/visualComponent.vue
+++ frontend/src/components/visual/visualComponent.vue
@@ -1,13 +1,16 @@
 <template>
   <div style="position: relative;">
     <div class="visual">
-      <img src="~@/assets/images/happyHouse.png" class="visual_img" alt="비주얼 이미지"><img/>
+      <img src="@/assets/images/happyHouse.png" class="visual_img" alt="비주얼 이미지"><img/>
     </div>
     <div class="searchBox_layer">
       <div class="searchBox_bg">
           <div class="searchBox">
             <form action="#" class="searchForm" ref="searchForm">
               <div class="searchBox_inputGroup">
+                <div>
+                  <img src="@/assets/images/searchIcon.png" alt="찾기 이미지">
+                </div>
                 <input type="text" autocomplete="off" placeholder="관심지역 또는 매물번호를 입력해 주세요" ref="keyword" v-on:keyup="getKeyword">
                 <!-- <input type="submit" value="매물검색"> -->
                 <span class="btn" v-on:click="submit">매물 검색</span>
@@ -36,6 +39,6 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped>
 
 </style>
