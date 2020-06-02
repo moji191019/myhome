e093b848545186ce73d3543c54aecaeb27163537
sungjong <tjdwhd3345@gmail.com>
2020-06-02 23:41:30 +0900

    mozzi / 검색목록 수정
    1. 디자인 수정
    2.
    3. 검색 직후 바로 반영되도록 수정 (this.$store.getters.FetchedList;)

--- frontend/src/components/visual/visualComponent.vue
+++ frontend/src/components/visual/visualComponent.vue
@@ -1,4 +1,3 @@
-/* eslint-disable eqeqeq */
 <template>
   <div style="position: relative;">
     <div class="visual">
@@ -22,18 +21,39 @@
           <template v-if="isFocus">
             <div class="autoCompleteWrap">
               <template v-if="isSearch">
-                <div class="autoCompleteContent"></div>
-                <div class="autoCompleteContent"></div>
+                <div class="autoCompleteContent">
+                  <div class="autoCompleteContentHeader">
+                    <img src="" alt="지역,지하철,대학교 아이콘">
+                    <span style="vertical-align: middle;">지역, 지하철, 대학교</span>
+                  </div>
+                  <ul class="listWrap">
+                    <li class="list" v-for="(apt, aptIdx) in getAptList" :key="aptIdx">
+                      <p>{{ apt.adres_city }} {{ apt.adres_gu }} {{ apt.adres_dong }}</p>
+                    </li>
+                  </ul>
+                </div>
+                <div class="autoCompleteContent">
+                  <div class="autoCompleteContentHeader">
+                    <img src="" alt="오피스텔 아이콘">
+                    <span style="vertical-align: middle;">오피스텔</span>
+                  </div>
+                  <ul class="listWrap">
+                    <li class="list" v-for="(apt, aptIdx) in getAptList" :key="aptIdx">
+                      <p>{{ apt.apt_nm }}</p>
+                      <p>{{ apt.adres_city }} {{ apt.adres_gu }} {{ apt.adres_dong }}</p>
+                    </li>
+                  </ul>
+                </div>
                 <div class="autoCompleteContent">
                   <div class="autoCompleteContentHeader">
                     <img src="" alt="아파트 아이콘">
                     <span style="vertical-align: middle;">아파트</span>
                   </div>
-                  <ul class="aptList">
-                    <li v-for="(apt, aptIdx) in getAptList" :key="aptIdx">
+                  <ul class="listWrap">
+                    <li class="list" v-for="(apt, aptIdx) in getAptList" :key="aptIdx">
                       <p>{{ apt.apt_nm }}</p>
                       <p>{{ apt.adres_city }} {{ apt.adres_gu }} {{ apt.adres_dong }}</p>
-                      <a> {{ apt.key1 }} {{ apt.key2 }} {{ apt.key3 }} ({{ apt.key4 }})</a>
+                      <!-- <a> {{ apt.key1 }} {{ apt.key2 }} {{ apt.key3 }} ({{ apt.key4 }})</a> -->
                       <!-- <a> {{ apt.adres_city }} {{ apt.adres_gu }} {{ apt.adres_doro }} ({{ apt.adres_dong }})</a> -->
                     </li>
                   </ul>
@@ -70,8 +90,6 @@
 </template>
 
 <script>
-// import { store } from '@/store/index';
-// import axios from 'axios';
 import com from '../../assets/common/common';
 
 export default {
@@ -79,7 +97,7 @@ export default {
 
   computed: {
     getAptList() {
-      return this.$data.aptList;
+      return this.$store.getters.FetchedList;
     },
   },
   watch: {
@@ -91,9 +109,10 @@ export default {
       // 여기선 왜 this.$store가 없을까요? -> 화살표함수를 쓰면 this를 못 쓰네요.
       this.$store.dispatch('FETCH_LIST', data)
         .then((res) => {
-          // console.log('fetch_list: ', res, this.$store.getters.FetchedList);
+          console.log('fetch_list: ', res, this.$store.getters.FetchedList);
           this.isSearch = true;
           this.$data.aptList = this.$store.getters.FetchedList;
+          console.log('$data.aptList: ', this.$data.aptList, '$store.getters: ', this.$store.getters.FetchedList);
           return res;
         })
         .catch((error) => { this.isSearch = true; console.error('fetch_list error: ', error); });
@@ -109,7 +128,7 @@ export default {
 
     handleFocus(e) {
       const s = e.target.parentElement.parentElement;
-      console.log(s);
+      // console.log(s);
       this.isFocus = true;
       document.getElementsByTagName('body')[0].scrollIntoView(true);
       s.classList.add('fff');
@@ -117,7 +136,7 @@ export default {
 
     handleBlur(e) {
       const s = e.target.parentElement.parentElement;
-      console.log(s);
+      // console.log(s);
       this.isFocus = false;
       this.isSearch = false;
       s.classList.remove('fff');
@@ -149,7 +168,7 @@ export default {
       searchText: '방 찾기',
       imageText: '비주얼 이미지',
       kk: '',
-      aptList: [
+      aptList: [/*
         { dorojuso: '서울특별시 강남구 삼성로 11', apt_nm: '디에이치 아너힐즈', adres_city: '서울', adres_gu: '강남구', adres_dong: '개포동', adres_doro: '삼성로', kaptaddr: '서울특별시 강남구 개포동 138 디에이치 아너힐즈' },
         { dorojuso: '서울특별시 성북구 한천로 713', apt_nm: '래미안퍼스트하이', adres_city: '서울', adres_gu: '성북구', adres_dong: '장위동', adres_doro: '한천로', kaptaddr: '서울특별시 성북구 장위동 173-114 래미안퍼스트하이' },
         { dorojuso: '서울특별시 강동구 고덕로 333', apt_nm: '고덕 그라시움 아파트', adres_city: '서울', adres_gu: '강동구', adres_dong: '고덕동', adres_doro: '고덕로', kaptaddr: '서울특별시 강동구 고덕동 212 고덕 그라시움 아파트' },
@@ -159,7 +178,7 @@ export default {
         { dorojuso: '서울특별시 구로구 연동로 234', apt_nm: '항동하버라인2단지', adres_city: '서울', adres_gu: '구로구', adres_dong: '항동', adres_doro: '연동로', kaptaddr: '서울특별시 구로구 항동  항동하버라인2단지' },
         { dorojuso: '서울특별시 동대문구 휘경로 60', apt_nm: '휘경동 sk뷰 아파트', adres_city: '서울', adres_gu: '동대문구', adres_dong: '휘경동', adres_doro: '휘경로', kaptaddr: '서울특별시 동대문구 휘경동 128-12 휘경동 sk뷰 아파트' },
         { dorojuso: '서울특별시 서초구 잠원로14길 32', apt_nm: '래미안 신반포 리오센트', adres_city: '서울', adres_gu: '서초구', adres_dong: '잠원동', adres_doro: '잠원로14길', kaptaddr: '서울특별시 서초구 잠원동 161 래미안 신반포 리오센트' },
-      ],
+      */],
     };
   },
 };
