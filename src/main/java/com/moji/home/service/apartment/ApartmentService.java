338f62a67c13240c401b7106e3668f592da0d4e0
JiYoonKim <badjiyoon1@gmail.com>
2020-08-03 23:25:35 +0900

    김지윤 / NA / NA / git ignore 수정 및 Json to Map Util 추가

--- src/main/java/com/moji/home/service/apartment/ApartmentService.java
+++ src/main/java/com/moji/home/service/apartment/ApartmentService.java
@@ -1,22 +1,17 @@
 package com.moji.home.service.apartment;
 
-import com.google.common.reflect.TypeToken;
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
 import com.moji.home.dao.apartment.ApartmentDAO;
 import com.moji.home.dto.apartment.ApartmentDTO;
 import com.moji.home.dto.apartment.ApartmentDTO2;
 import com.moji.home.dto.request.ReqDTO;
 import com.moji.home.dto.response.ResDTO;
-
+import com.moji.home.util.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.FileReader;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ApartmentService {
@@ -45,21 +40,23 @@ public class ApartmentService {
         
         // json file을 읽어서 gson으로 객체변환하고 insert
         String filename = "C:\\Users\\tjdwh\\Downloads\\seoul_apt_info.json";
-        Gson gson = new Gson();
-        JsonReader reader = new JsonReader(new FileReader(filename));
-        System.out.println("######## JsonReader: " + reader.toString());
-        
-        Type ApartmentDTOListType = new TypeToken<ArrayList<ApartmentDTO>>() {}.getType();
-        Type ApartmentDTOListType2 = new TypeToken<ArrayList<ApartmentDTO2>>() {}.getType();
-        ArrayList<ApartmentDTO> ApartmentArray= gson.fromJson(reader, ApartmentDTOListType);
-        ArrayList<ApartmentDTO2> ApartmentArray2= gson.fromJson(reader, ApartmentDTOListType2);
+        Map<String, Object> apartmentMap = JsonUtil.createHashMapFromJsonString(filename);
 
-        for (ApartmentDTO apartment : ApartmentArray) {
-            System.out.println(apartment);
-        }
-        for (ApartmentDTO2 apartment : ApartmentArray2) {
-            System.out.println(apartment);
-        }
+//        Gson gson = new Gson();
+//        JsonReader reader = new JsonReader(new FileReader(filename));
+//        System.out.println("######## JsonReader: " + reader.toString());
+//
+//        Type ApartmentDTOListType = new TypeToken<ArrayList<ApartmentDTO>>() {}.getType();
+//        Type ApartmentDTOListType2 = new TypeToken<ArrayList<ApartmentDTO2>>() {}.getType();
+//        ArrayList<ApartmentDTO> ApartmentArray= gson.fromJson(reader, ApartmentDTOListType);
+//        ArrayList<ApartmentDTO2> ApartmentArray2= gson.fromJson(reader, ApartmentDTOListType2);
+//
+//        for (ApartmentDTO apartment : ApartmentArray) {
+//            System.out.println(apartment);
+//        }
+//        for (ApartmentDTO2 apartment : ApartmentArray2) {
+//            System.out.println(apartment);
+//        }
 
         // List<ApartmentDTO> data = gson.fromJson(reader, ApartmentDTO.class);
         // List<ApartmentDTO2> data2 = gson.fromJson(reader, ApartmentDTO2.class);
--- src/main/java/com/moji/home/util/JsonUtil.java
+++ src/main/java/com/moji/home/util/JsonUtil.java
@@ -1,13 +1,15 @@
 package com.moji.home.util;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.HashMap;
+import java.util.*;
 
 public class JsonUtil {
+    private static JsonParser parser = new JsonParser();
+
     public static HashMap readJsonFileToMap (String path) {
         HashMap<String, String> json = null;
         Gson gson = new Gson();
@@ -19,4 +21,39 @@ public class JsonUtil {
         }
         return json;
     };
+
+    public static HashMap<String, Object> createHashMapFromJsonString(String jsonStr) {
+        JsonObject jsonObject = (JsonObject) parser.parse(jsonStr);
+        Set<Map.Entry<String, JsonElement>> set = jsonObject.entrySet();
+        Iterator<Map.Entry<String, JsonElement>> iterator = set.iterator();
+        HashMap<String, Object> map = new HashMap<String, Object>();
+
+        while(iterator.hasNext()) {
+            Map.Entry<String, JsonElement> entry = iterator.next();
+            String key = entry.getKey();
+            JsonElement value = entry.getValue();
+
+            if (null != value) {
+                if (!value.isJsonPrimitive()) {
+                    if (value.isJsonObject()) {
+                        map.put(key, createHashMapFromJsonString(value.toString()));
+                    } else if (value.isJsonArray() && value.toString().contains(":")) {
+                        List<HashMap<String, Object>> list = new ArrayList<>();
+                        JsonArray array = value.getAsJsonArray();
+                        if (null != array) {
+                            for (JsonElement element : array) {
+                                list.add(createHashMapFromJsonString(element.toString()));
+                            }
+                            map.put(key, list);
+                        }
+                    } else if (value.isJsonArray() && !value.toString().contains(":")) {
+                        map.put(key, value.getAsJsonArray());
+                    }
+                } else {
+                    map.put(key, value.getAsString());
+                }
+            }
+        }
+        return map;
+    }
 }
